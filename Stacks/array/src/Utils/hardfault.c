d92a7ebef7c21e9ba68599d3461bb7446259908b
Andy Pomfret <andrew.pomfret@york.ac.uk>
2024-09-21 11:42:42 +0100

    Align with ELE00138M-files for 2024

--- Stacks/array/src/Utils/hardfault.c
+++ Stacks/array/src/Utils/hardfault.c
@@ -13,49 +13,65 @@
  */
 
 void HardFault_HandlerC(uint32_t *hardfault_args) {
-	volatile uint32_t stacked_r0 ;
-	volatile uint32_t stacked_r1 ;
-	volatile uint32_t stacked_r2 ;
-	volatile uint32_t stacked_r3 ;
-	volatile uint32_t stacked_r12 ;
-	volatile uint32_t stacked_lr ;
-	volatile uint32_t stacked_pc ;
-	volatile uint32_t stacked_psr ;
-	volatile uint32_t _CFSR ;
-	volatile uint32_t _HFSR ;
-	volatile uint32_t _DFSR ;
-	volatile uint32_t _AFSR ;
-	volatile uint32_t _BFAR ;
-	volatile uint32_t _MMAR ;
+	volatile uint32_t stacked_r0;
+	volatile uint32_t stacked_r1;
+	volatile uint32_t stacked_r2;
+	volatile uint32_t stacked_r3;
+	volatile uint32_t stacked_r12;
+	volatile uint32_t stacked_lr;
+	volatile uint32_t stacked_pc;
+	volatile uint32_t stacked_psr;
+	volatile uint32_t _CFSR;
+	volatile uint32_t _HFSR;
+	volatile uint32_t _DFSR;
+	volatile uint32_t _AFSR;
+	volatile uint32_t _BFAR;
+	volatile uint32_t _MMAR;
 
-	stacked_r0  = ((uint32_t)hardfault_args[0]) ;
-	stacked_r1  = ((uint32_t)hardfault_args[1]) ;
-	stacked_r2  = ((uint32_t)hardfault_args[2]) ;
-	stacked_r3  = ((uint32_t)hardfault_args[3]) ;
-	stacked_r12 = ((uint32_t)hardfault_args[4]) ;
-	stacked_lr  = ((uint32_t)hardfault_args[5]) ;
-	stacked_pc  = ((uint32_t)hardfault_args[6]) ;
-	stacked_psr = ((uint32_t)hardfault_args[7]) ;
+	stacked_r0  = ((uint32_t)hardfault_args[0]);
+	stacked_r1  = ((uint32_t)hardfault_args[1]);
+	stacked_r2  = ((uint32_t)hardfault_args[2]);
+	stacked_r3  = ((uint32_t)hardfault_args[3]);
+	stacked_r12 = ((uint32_t)hardfault_args[4]);
+	stacked_lr  = ((uint32_t)hardfault_args[5]);
+	stacked_pc  = ((uint32_t)hardfault_args[6]);
+	stacked_psr = ((uint32_t)hardfault_args[7]);
+	
+	// Suppress "set but not used" warnings
+	(void) stacked_r0;
+	(void) stacked_r1;
+	(void) stacked_r2;
+	(void) stacked_r3;
+	(void) stacked_r12;
+	(void) stacked_lr;
+	(void) stacked_pc;
+	(void) stacked_psr;
+	(void) _CFSR;
+	(void) _HFSR;
+	(void) _DFSR;
+	(void) _AFSR;
+	(void) _BFAR;
+	(void) _MMAR;
 
 	// Configurable Fault Status Register
 	// Consists of MMSR, BFSR and UFSR
-	_CFSR = (*((volatile uint32_t *)(0xE000ED28))) ;   
+	_CFSR = (*((volatile uint32_t *)(0xE000ED28)));   
 																																									
 	// Hard Fault Status Register
-	_HFSR = (*((volatile uint32_t *)(0xE000ED2C))) ;
+	_HFSR = (*((volatile uint32_t *)(0xE000ED2C)));
 
 	// Debug Fault Status Register
-	_DFSR = (*((volatile uint32_t *)(0xE000ED30))) ;
+	_DFSR = (*((volatile uint32_t *)(0xE000ED30)));
 
 	// Auxiliary Fault Status Register
-	_AFSR = (*((volatile uint32_t *)(0xE000ED3C))) ;
+	_AFSR = (*((volatile uint32_t *)(0xE000ED3C)));
 
 	// Read the Fault Address Registers. These may not contain valid values.
 	// Check BFARVALID/MMARVALID to see if they are valid values
 	// MemManage Fault Address Register
-	_MMAR = (*((volatile uint32_t *)(0xE000ED34))) ;
+	_MMAR = (*((volatile uint32_t *)(0xE000ED34)));
 	// Bus Fault Address Register
-	_BFAR = (*((volatile uint32_t *)(0xE000ED38))) ;
+	_BFAR = (*((volatile uint32_t *)(0xE000ED38)));
 
 	__BKPT(0);
 }
