21359764436d274ceebcc3cbcbec204c210d20bf
Jonathan Cooke <jac696@its.york.ac.uk>
2024-09-25 18:13:07 +0100

    Lab work from Wednesday 25th in P/T/301. Completed Types, Float and Pointers exercises.

--- Programming/pointers/src/main.c
+++ Programming/pointers/src/main.c
@@ -6,8 +6,23 @@ int main(void) {
 	configClock();
 	configUSART2(38400);
 	
-	int i = 3;
+	void square(long long *value);
+	
+	long long i = 3;
+	
+	printf("---RESET---\r\n\r\n");
+	
+	printf("Memory Address of i: (&i) = 0x%p \r\n", &i);
+	printf("(&i) + 1 = 0x%p \r\n", (void *)((&i) + 1));
+	
+	printf("i = %lld\r\n", i);
+	printf("square(&i)\r\n");
+	square(&i);
+	printf("i = %lld\r\n", i);
 	
 	while(1);
+}
 
+void square(long long *value){
+	*value = *value**value;
 }
--- Programming/types/src/main.c
+++ Programming/types/src/main.c
@@ -6,7 +6,21 @@ int main(void) {
 	configClock();
 	configUSART2(38400);
 	
-	printf("char: %d\r\n", sizeof(char));
+	printf("---RESET---\r\n");
+	
+	printf("bits: %d\r\n", sizeof(char)*8);
+	
+	int x = 42000;
+	short int y;
+	
+	y = (short int) x;
+	
+	printf("x = %d, y = %hd\r\n", x, y);
+	
+	printf("---Literals---\r\n");
+	
+	unsigned int mask = (0xBAUL) << 24;
+	printf("mask: 0x%08X\r\n", mask);
 
 	while(1);
 }
