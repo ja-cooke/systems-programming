70ed89f60b9dd7d5027683c0e118cddb70c4c521
Jonathan Cooke <jac696@york.ac.uk>
2024-10-14 10:47:26 +0100

    Started work on Allocation. Not yet functioning static allocator.

---
+++ Allocation/static/Objects/static_alloc.d
@@ -0,0 +1,1 @@
+./objects/static_alloc.o: src\static_alloc.c inc\static_alloc.h
--- Allocation/static/src/static_alloc.c
+++ Allocation/static/src/static_alloc.c
@@ -8,5 +8,24 @@ static uint8_t static_pool[STATIC_ALLOC_POOLSIZE];
 static size_t pool_index = STATIC_ALLOC_POOLSIZE;
 
 void * static_alloc(size_t bytes) {
-	return 0;
+	/* 
+	* Check that the requested number of bytes is available in the array. This is 
+	* easy because the pool array index decreases: if the index is greater than
+	* the number of requested bytes, all is fine. If not, zero should be returned
+	* to indicate failure.
+	*/
+	
+	size_t * pool_index_ptr = &pool_index;
+	size_t callpoint_pool_index = *pool_index_ptr;
+	
+	if(pool_index < bytes){
+		return 0;
+	}
+	else{
+		// Reduce the pool array index by the requested number of bytes.
+		*pool_index_ptr = *pool_index_ptr - bytes;
+		
+		// Return the new pool array index.
+	return pool_index_ptr;
+	}
 }
