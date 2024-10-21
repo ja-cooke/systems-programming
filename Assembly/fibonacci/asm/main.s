d0c2c372a2b11b647e1a64176f9395107ce9b15a
Jonathan Cooke <jac696@york.ac.uk>
2024-10-21 10:51:39 +0100

    Started Lab 6 ARMASM work, still need to refactor Lab5 for final task.

--- Assembly/fibonacci/asm/main.s
+++ Assembly/fibonacci/asm/main.s
@@ -7,6 +7,14 @@ array_end
 	EXPORT asm_main
 asm_main
 	; Your code goes here
+	LDR r0, =array
+	LDR r1, =array_end
+	
+	MOV r2, #1
+	MOV r3, #1
+	
+loop
+	STR r3, [r0], #4
 
 	B .			; Loop forever
 
--- Assembly/mult/asm/main.s
+++ Assembly/mult/asm/main.s
@@ -2,7 +2,18 @@
 	EXPORT asm_main
 asm_main
 
-	; Your instructions go here...
+	; Inititilisations
+	MOV r0, #2
+	MOV r1, #0
+	
+	; Simple routine to multiply 10 by 13 and store it in r1
+	;MOV r1, r0, LSL #4 		; R1 = R0 * 16
+	;SUB r1, r1, r0, LSL #1 	; R1 = R1 - R0 * 2 = R0 *14
+	;SUB r1, r1, r0				; R1 = R1 - R0 = R0 * 13
+	
+	; Solution from SPIRAL
+	ADD r1, r0, r0, LSL #4		; R1 = R0 + R0 * 16 = R0 * 17
+	SUB r1, r1, r0, LSL #2		; R1 = R1 - R0 * 4 = R0*17 - R0*4 = R0 * 13
 
 	B .			; Loop forever
 
