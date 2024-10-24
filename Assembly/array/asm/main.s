44f3ea18ab860ab4ad8a558b28f24cad099a15b0
Jonathan Cooke <jac696@york.ac.uk>
2024-10-24 15:55:25 +0100

    All sections up until 6.5.1 More Complexity are now complete.

--- Assembly/array/asm/main.s
+++ Assembly/array/asm/main.s
@@ -1,15 +1,42 @@
 	AREA storage,DATA
 array
 	; Total is 232 for the example list
-	DCD 3, 7, 31, 193, 2, -5, 1
+	;DCD 1, 7, 31, 193, 2, -5, 1
+	DCD 5456, 894, 295, -456, -8754, -89, 6, -78, 5, 9
 array_end
 
 	AREA main,CODE
 	EXPORT asm_main
+
+mod
+	; do something
+	CMP r2, #0
+	BMI negative
+	BX LR
+	
+negative
+	MVN r2, r2
+	ADD r2, #1
+	BX LR
+
 asm_main
 	; Your code goes here
-
+	LDR r0, =array
+	LDR r1, =array_end
  
+	LDR r2, [r0], #4
+	BL mod
+	MOV r3, r2
+	
+loop
+	LDR r2, [r0], #4
+	BL mod
+	
+	ADD r3, r2, r3
+	
+	CMP r0, r1
+	BLT loop
+	
 	B .			; Loop forever
 
 	ALIGN
--- Assembly/fibonacci/asm/main.s
+++ Assembly/fibonacci/asm/main.s
@@ -1,6 +1,6 @@
 	AREA storage,DATA
 array
-	SPACE 4*20	; Declares a 20-word storage area
+	SPACE 4*21	; Declares a 20-word storage area
 array_end
 
 	AREA mainarea,CODE
@@ -13,8 +13,15 @@ asm_main
 	MOV r2, #1
 	MOV r3, #1
 	
+	STMIA r0!, {r2, r3}
+	
 loop
-	STR r3, [r0], #4
+	ADD r2, r2, r3
+	ADD r3, r2, r3
+	STMIA r0!, {r2, r3}
+	
+	CMP r0, r1
+	BCC loop
 
 	B .			; Loop forever
 
