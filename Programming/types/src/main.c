70ca69e5a4b7f6644d99ddd2ccf4c707ed4879e5
ja-cooke <jac696@york.ac.uk>
2024-10-28 17:16:27 +0000

    Just some leftovers from when I was first starting the labs I think. Most of this has probably been superceeded.

--- Programming/types/src/main.c
+++ Programming/types/src/main.c
@@ -8,11 +8,13 @@ int main(void) {
 	
 	printf("char: %d\r\n", sizeof(char));
 	
-	int x = 42;
-	unsigned int y = 42;
+	int x = -42;
+	unsigned int y = -42;
 	
-	y = x;
+	y = (unsigned int) x;
 	(void) y;
+	
+	printf("x = %d, y = %u", x, y);
 	
 	while(1);
 }
