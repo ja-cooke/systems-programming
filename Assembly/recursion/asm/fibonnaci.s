28d80e06b6e1df53235dc653ab5b40865256c2c8
ja-cooke <jac696@york.ac.uk>
2024-11-04 00:40:44 +0000

    Lab 6 fully complete, working solution to recursion.

--- Assembly/recursion/asm/fibonnaci.s
+++ Assembly/recursion/asm/fibonnaci.s
@@ -1,51 +1,33 @@
-	AREA storage,DATA
-array
-	SPACE 16*31	; Declares a 30x 32-bit storage area
-array_end
-	
 	AREA mainarea,CODE
 	EXPORT fib		
 		
-branch
-	SUB r0, r2, #1
-	
-	MOV r3, r4
-	MOV r2, r3
-	
-	B fib 	; Call to find n-1
-	
-	MOV r3, r5
-	
-	B fib 	; Call to find n-2
-	
-	MOV r4, r5
-	ADD r2, r3, r4
-	
-	LDR r1, [r6], #-4
-	MOV LR, r1
-	BX LR		; exit
-	
-arrayStore
-
-arrayEnd
-		
 fib
-	LDR r6, =array
-	LDR r7, =array_end
-
-	MOV r1, LR
-	
-	STR r1, [r6], #4
-	
-	MOV r2, r0
-	
 	CMP r0, #2
 	BHI branch
 	
-	MOV r5, #1
+	MOV r1, #1
 	
-	LDR r1, [r6], #-4
-	MOV LR, r1
-	BX LR 		; exit
+	BX	lr; exit
+		
+branch
+	SUB r0, #1
+	
+	STMFD sp!, {r0,r4-r5,lr}
+	BL fib 	; Call to find n-1
+	LDMFD sp!, {r0,r4-r5,lr}
+	
+	MOV r4, r1
+	
+	SUB r0, #1
+	
+	STMFD sp!, {r0,r4-r5,lr}
+	BL fib 	; Call to find n-2
+	LDMFD sp!, {r0,r4-r5,lr}
+	
+	MOV r5, r1
+	
+	ADD r1, r4, r5
+	
+	BX 	lr; exit
 	
 	END
--- Assembly/recursion/asm/main.s
+++ Assembly/recursion/asm/main.s
@@ -8,9 +8,7 @@ asm_main
 	MOV r0, #7
 	
 	BL fib 		; Your call to 'fib' goes here
-
-	MOV r8, r0
-	MOV r9, r0
+	MOV r0, r1
 
 	B .			; Loop forever
 
