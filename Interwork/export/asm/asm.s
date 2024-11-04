24b3832b8c16ab1fa86432477841c1706e46e284
Jonathan Cooke <jac696@york.ac.uk>
2024-11-04 11:56:31 +0000

    Export and Import tasks completed, fib function doesn't actually work correctly, as it must accept even numbers only. However, proof of out-of-line C concepts is given.

--- Interwork/export/asm/asm.s
+++ Interwork/export/asm/asm.s
@@ -1,5 +1,14 @@
 	AREA mainarea,CODE
+	EXPORT calculate
 
-	; Your instructions go here...
-
+	; 5x + y
+	; 5.r0 + r1
+calculate
+	ADD r3, r0, r0, LSL #2
+	ADD r3, r3, r1
+	
+	MOV r0, r3
+	
+	BX LR
+	
 	END
--- Interwork/import/asm/asm.s
+++ Interwork/import/asm/asm.s
@@ -1,5 +1,35 @@
+	AREA storage,DATA
+array
+	SPACE 4*19	; Declares a 20-word storage area
+array_end
+	
 	AREA mainarea,CODE
+	IMPORT report
+	EXPORT fib
+	
+fib
+	STMFD sp!, {r4-r6, lr}
 
-	; Your instructions go here...
+	LDR r4, =array
+	LDR r5, =array_end
+	
+	MOV r2, #1
+	MOV r3, #1
+	
+	STMIA r4!, {r2, r3}
+	
+loop
+	ADD r2, r2, r3
+	ADD r3, r2, r3
+	STMIA r4!, {r2, r3}
+	
+	CMP r4, r5
+	BCC loop
+	
+	MOV r0, r3
+	
+	BL report
 
+	LDMFD sp!, {r4-r6, lr}
+	
 	END
