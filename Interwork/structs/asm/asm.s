8e6b9a165d32b6d5e0c2e8c015cfd7a81fcf8ac6
ja-cooke <jac696@york.ac.uk>
2024-11-05 20:50:07 +0000

    Completed Interwork section 7.4.1. Callback function: Make It Work.

--- Interwork/structs/asm/asm.s
+++ Interwork/structs/asm/asm.s
@@ -1,5 +1,31 @@
 	AREA mainarea,CODE
+	EXPORT process
 
-	; Your instructions go here...
+process
+	; preserve clobbered registers
+	STMFD sp!, {r4-r8,lr}
+	
+	; Extract the input argument
+	MOV r4, r0
+	
+	; Load C struct contents into registers
+	LDR r5, [r4]
+	LDR r6, [r4, #4]
+	LDR r7, [r4, #8]
+	LDR r8, [r4, #12]
+	
+	; Add integer data in structure and return it
+	ADD r6, r5, r6
+	STR r6, [r4, #4]
+	
+	; Call the callback function 
+	; Input argument ptr
+	MOV r0, r7
+	BLX r8
+	
+	; Restore registers from stack
+	LDMFD sp!, {r4-r8,lr}
+	; Return to main.c
+	BX lr
 
 	END
--- Interwork/structs/src/main.c
+++ Interwork/structs/src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <inttypes.h>
 #include "Utils/utils.h"
 
 #pragma pack(push)
@@ -14,11 +15,29 @@ typedef struct {
 
 #pragma pack(pop)
 
+void process(structure_t * s);
+
+void printInteger(void * integer);
+
 int main(void) {
 	configClock();
 	configUSART2(38400);
 
-	// Your code goes here...
+	structure_t blade_runner = {.x=20, .y=49};
+	
+	int16_t doomsday = 2808;
+	
+	blade_runner.ptr = &doomsday;
+	blade_runner.callback = &printInteger;
 
+	process(&blade_runner);
+
+	printf("Value of x: %d -- Value of y: %d\r\n", blade_runner.x, blade_runner.y);
+	
 	while(1);
+}
+
+void printInteger(void * integer) {
+	int16_t * local_int = (int16_t*) integer;
+	printf("The value of the integer is: %" PRId16 "\r\n", *local_int);
 }
