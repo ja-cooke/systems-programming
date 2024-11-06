8a510dd18678e41fa5b58c12e8fa009f80eac486
ja-cooke <jac696@york.ac.uk>
2024-11-06 15:06:44 +0000

    Finished Lab 7 early oops. All Inline Assembly exercises complete. Recommended to return to earlier labs 6 and 7 to pick up exercises which could have had more comprehensive solutions.

--- Interwork/inline/src/main.c
+++ Interwork/inline/src/main.c
@@ -7,15 +7,26 @@ int main(void) {
 	configClock();
 	configUSART2(38400);
 
-	uint32_t x=3, y=5, z=0;
+//	uint32_t x=3, y=5, z =0;
+//  __asm volatile (
+//    "ADD %[output1], %[input1], %[input1], LSL #3\n\t" /* x*9 */
+//    "ADD %[output1], %[output1], %[input1], LSL #2\n\t" /* +(x*4) */
+//		"ADD %[output2], %[output1], %[input2]" /* 13x + y */
+//    : [output1] "+&r" (y), [output2] "=&r" (z)
+//    : [input1] "r" (x), [input2] "r" (y)
+//	);
+	
+		uint32_t x=3, y=5;
   __asm volatile (
-    "ADD %[output], %[input], %[input], LSL #3\n\t" /* x*9 */
-    "ADD %[output], %[output], %[input], LSL #2" /* +(x*4) */
-    : [output] "=&r" (y)
-    : [input] "r" (x)
+    "ADD r4, %[input1], %[input1], LSL #3\n\t" /* x*9 */
+    "ADD r4, r4, %[input1], LSL #2\n\t" /* +(x*4) */
+		"ADD %[output1], r4, %[input2]" /* 13x + y */
+    : [output1] "=&r" (y)
+    : [input1] "r" (x), [input2] "r" (y)
+		: "r4"
 	);
 
-	printf("z = %" PRIu32 "\n", z);
+	printf("y = %" PRIu32 "\n", y);
 
 	while(1);
 }
