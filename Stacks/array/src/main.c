b94a2ae4fde20f2551edfb0acafa988f94a75e32
Jonathan Cooke <jac696@york.ac.uk>
2024-11-11 11:04:09 +0000

    Started on Lab 8 work. Stacks -- array.

--- Stacks/array/src/main.c
+++ Stacks/array/src/main.c
@@ -9,8 +9,26 @@ int main(void) {
 	configClock();
 	configUSART2(38400);
 
+	printf("---RESET---\r\n");
+	printf("Test() test\r\n");
+	test();
+	printf("---Overwite Test---\r\n");
+	
 	uint32_t array[10];
 	
+	for(uint32_t i=0;i<10;i++){
+		
+	array[i]=i+1;
 	// First try overflowing the stack with garbage and see what happens
 	// Then try to overwrite the link register here so that test() is run
+
+	}
+	
+	//*(array+9)= 9;
+	//*(array+10)= 9;
+	//*(array+11)= 9;
+	*(array+10)= 34240008; // This causes the most problems
+	//*(array+13)= 9;
+	//*(array+14)= 9;
+	
 }
