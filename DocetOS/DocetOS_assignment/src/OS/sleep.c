9ecc04cc550de17f495c57986c81bd905e45c8ad
ja-cooke <jac696@york.ac.uk>
2025-01-18 14:54:59 +0000

    Improved the safety of heap sorting at wakeTime overflows, by forcing overflows to -1UL. However, this means all tasks wake up when SysTick overflows.

--- DocetOS/DocetOS_assignment/src/OS/sleep.c
+++ DocetOS/DocetOS_assignment/src/OS/sleep.c
@@ -7,8 +7,26 @@ void OS_sleep(uint32_t sleepTime){
 	uint32_t currentTime = OS_elapsedTicks();
 	OS_TCB_t * currentTCB = OS_currentTCB();
 	
-	// Store the next wake time
-	currentTCB->data = currentTime + sleepTime;
+	uint32_t wakeTime = currentTime + sleepTime;
+	
+	/* Check for overflows */
+	if (wakeTime > currentTime) {
+		// Store the next wake time
+		currentTCB->data = currentTime + sleepTime;
+	}
+	else {
+		/* If the wake time has overflowed, the time is set to the last time before
+		 * SysTick overflows. This ensures that sleeping tasks are never risen to the
+		 * top of the sleepHeap prematurely which would cause sleeping tasks to wake 
+		 * at the next SysTick after they are put to sleep.
+		 * This implementation means that all tasks will wake when SysTick overflows
+		 * however this full sleep reset design is safe from tasks being put
+		 * permanently to sleep due to overflows.
+		 */
+		currentTCB->data = (uint32_t)(-1L);
+	}
+	
+	
 	
 	// Set the status to sleep
 	currentTCB->state |= TASK_STATE_SLEEP; // Need to check that this works
